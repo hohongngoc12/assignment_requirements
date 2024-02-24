104c3e5929780e01d7e823371765d144ad626f71
hohongngoc12 <149927056+hohongngoc12@users.noreply.github.com>
2024-02-24 17:31:47 +0700

    bao_dcs.py

--- bao_dcs.py
+++ bao_dcs.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
 
 def GetSoup(url):
     response = requests.get(url)
@@ -8,9 +9,35 @@ def GetSoup(url):
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup
 
+def GetContent(url):
+    try:
+        print(f"Fetching content for URL: {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            html_content = response.content
+            soup = BeautifulSoup(html_content, 'html.parser')
+            content_div = soup.find('div', class_='post-content')
+            if content_div:
+                text_content = content_div.get_text(separator='\n')
+                # Clean up text content
+                text_content = text_content.strip()
+                text_content = re.sub(r'\n+', '\n', text_content)  # Remove excessive newline characters
+                return text_content.strip()
+            else:
+                print(f"Content div not found for: {url}")
+                return None
+        else:
+            print(f"Failed to fetch content for: {url}. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Error fetching content for {url}: {e}")
+        return None
+
+
+
 data = {}
 
-for i in range(2):  # Lặp qua 2 trang
+for i in range(5):  # Loop through the desired number of pages
     url = f"https://dangcongsan.vn/tu-tuong-van-hoa/p{i}"
     soup = GetSoup(url)
     articles = soup.find_all('div', class_='col-md-4 subnews-item')
@@ -19,8 +46,7 @@ for i in range(2):  # Lặp qua 2 trang
         try:
             title = article.find('a')['title']
             link = article.find('a')['href']
-            summary = article.find('div', class_='summary')
-            content = summary.text.strip() if summary else None
+            content = GetContent(link)
 
             data[f"Article_{i * len(articles) + idx}"] = {
                 'Title': title,
@@ -30,8 +56,8 @@ for i in range(2):  # Lặp qua 2 trang
         except Exception as e:
             print(f"Error: {e}")
 
-# Lưu dữ liệu vào tệp JSON
-with open('dcs_data.json', 'w', encoding='utf-8') as f:
+# Save data to a JSON file
+with open('baodcs_data.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
-print("Data saved to 'dcs_data.json' file.")
+print("Data saved to 'baodcs_data.json' file.")
